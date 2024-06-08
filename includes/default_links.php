eb5398d5f9981cc9861e97e1e9e14aca626e3530
19leunam93 <tech.spuur@quickline.ch>
2024-06-08 11:57:44 +0200

    add styles

--- includes/default_links.php
+++ includes/default_links.php
@@ -17,6 +17,8 @@
  * @since  4.5.0
  */
 $default_links_array = [
+    "name"        => "joomla.org",
+    "level"       => "0",
     "jug"         => "https://community.joomla.org/user-groups",
     "forum"       => "https://forum.joomla.org",
     "jday"        => "https://community.joomla.org/events?filter[calendars][1]=97",
@@ -24,6 +26,6 @@ $default_links_array = [
     "vportal"     => "https://volunteers.joomla.org",
     "geolocation" => "51.5000,0.0000",
     "news_feed"   => "https://community.joomla.org/blogs?format=feed&type=rss",
-    "events_feed" => "https://community.joomla.org/events",
+    "events_feed" => "https://community.joomla.org/events?format=feed&type=ical",
     "newsletter"  => "https://community.joomla.org/general-newsletter"
-]; 
+];
--- tmpl/default.php
+++ tmpl/default.php
@@ -17,7 +17,8 @@ use Joomla\Module\CommunityInfo\Administrator\Helper\CommunityInfoHelper;
 
 /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
 $wa = $app->getDocument()->getWebAssetManager();
-//$wa->registerAndUseScript('communityinfo.leaflet', Uri::base(true).'administrator/modules/mod_community_info/media/js/leaflet.js');
+$wa->useStyle('communityinfo.style');
+$wa->useStyle('communityinfo.leaflet');
 $wa->useScript('communityinfo.leaflet');
 $wa->useScript('bootstrap.modal');
 $wa->addInlineStyle('#map * + * {margin: 0;}');
@@ -28,74 +29,87 @@ $currentURL   = Uri::getInstance()->toString();
 ?>
 
 <div id="CommunityInfo<?php echo strval($module->id); ?>" class="mod-community-info px-3">
-  <a href="#" onclick="openModal('location-modal')">
-    <i class="icon-location"></i>
-    <?php echo Text::_('MOD_COMMUNITY_INFO_CHOOSE_LOCATION'); ?>
-  </a><span> (<?php echo Text::_('JCURRENT'); ?>: <?php echo CommunityInfoHelper::getLocation($params, 'label'); ?>)</span>
   <p><?php echo Text::_('MOD_COMMUNITY_INFO_JOOMLA_DESC'); ?></p>
   <hr />
-  <h3><?php echo Text::_('MOD_COMMUNITY_INFO_CONTACT_TITLE'); ?></h3>
-  <p><?php echo CommunityInfoHelper::replaceText(Text::_('MOD_COMMUNITY_INFO_CONTACT_TEXT'), $links); ?></p>
+  <div class="info-block contact">
+    <h3><?php echo Text::_('MOD_COMMUNITY_INFO_CONTACT_TITLE'); ?></h3>
+    <p><?php echo CommunityInfoHelper::replaceText(Text::_('MOD_COMMUNITY_INFO_CONTACT_TEXT'), $links); ?></p>
+  </div>  
   <hr />
-  <h3><?php echo Text::_('MOD_COMMUNITY_INFO_CONTRIBUTE_TITLE'); ?></h3>
-  <p><?php echo CommunityInfoHelper::replaceText(Text::_('MOD_COMMUNITY_INFO_CONTRIBUTE_TEXT'), $links); ?></p>
-  <p><?php echo CommunityInfoHelper::replaceText(Text::_('MOD_COMMUNITY_INFO_CONTRIBUTE_CONTACT'), $links); ?></p>
+  <div class="info-block contribute">
+    <h3><?php echo Text::_('MOD_COMMUNITY_INFO_CONTRIBUTE_TITLE'); ?></h3>
+    <p><?php echo CommunityInfoHelper::replaceText(Text::_('MOD_COMMUNITY_INFO_CONTRIBUTE_TEXT'), $links); ?></p>
+    <p><?php echo CommunityInfoHelper::replaceText(Text::_('MOD_COMMUNITY_INFO_CONTRIBUTE_CONTACT'), $links); ?></p>
+  </div>
   <hr />
-  <h3><?php echo Text::_('MOD_COMMUNITY_INFO_NEWS_TITLE'); ?></h3>
-  <p><?php echo Text::_('MOD_COMMUNITY_INFO_NEWS_INTRO'); ?></p>
-  <a class="btn btn-primary mb-3"><?php echo Text::_('MOD_COMMUNITY_INFO_NEWS_SUBSCRIBE'); ?></a>
-  <table class="table community-info-news">
-    <tbody>
-      <tr>
-        <td scope="row"><a href="https://www.joomla.de/news/joomla/joomla-5-1-0-und-joomla-4-4-4-veroeffentlicht" target="_blank">Hurra, Joomla 5.1.0 und Joomla! 4.4.4 sind da</a></td>
-      </tr>
-      <tr>
-        <td><a href="https://www.joomla.de/news/joomla/joomla-5-1-release-candidate-veroeffentlicht" target="_blank">Joomla! 5.1 Release Candidate veröffentlicht</a></td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="info-block news">
+    <div class="intro-txt">
+      <div>
+        <h3><?php echo Text::_('MOD_COMMUNITY_INFO_NEWS_TITLE'); ?></h3>
+        <p><?php echo Text::_('MOD_COMMUNITY_INFO_NEWS_INTRO'); ?></p>
+      </div>
+      <a class="btn btn-primary mb-3"><?php echo Text::_('MOD_COMMUNITY_INFO_NEWS_SUBSCRIBE'); ?></a>
+    </div>
+    <table class="table community-info-news">
+      <tbody>
+        <tr>
+          <td scope="row"><a href="https://www.joomla.de/news/joomla/joomla-5-1-0-und-joomla-4-4-4-veroeffentlicht" target="_blank">Hurra, Joomla 5.1.0 und Joomla! 4.4.4 sind da</a></td>
+        </tr>
+        <tr>
+          <td><a href="https://www.joomla.de/news/joomla/joomla-5-1-release-candidate-veroeffentlicht" target="_blank">Joomla! 5.1 Release Candidate veröffentlicht</a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>  
   <hr />
-  <h3><?php echo Text::_('MOD_COMMUNITY_INFO_EVENTS_TITLE'); ?></h3>
-  <p><?php echo Text::_('MOD_COMMUNITY_INFO_EVENTS_INTRO'); ?></p>
-  <table class="table community-info-news">
-    <tbody>
-      <tr>
-        <td scope="row">JUG Trimbach<br />Meetup - Trimbach, Schweiz</td>
-        <td style="text-align: right">Montag, Jun 3, 2024<br />19:00 GMT+2</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="info-block events">
+    <h3><?php echo Text::_('MOD_COMMUNITY_INFO_EVENTS_TITLE'); ?></h3>
+    <p><?php echo Text::_('MOD_COMMUNITY_INFO_EVENTS_INTRO'); ?></p>
+    <table class="table community-info-news">
+      <tbody>
+        <tr>
+          <td scope="row">JUG Trimbach<br />Meetup - Trimbach, Schweiz</td>
+          <td style="text-align: right">Montag, Jun 3, 2024<br />19:00 GMT+2</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </div>
+
+<template id="template-location-picker">
+  <div class="select-location">
+    <a href="#" onclick="openModal('location-modal')">
+      <i class="icon-location"></i>
+      <?php echo Text::_('MOD_COMMUNITY_INFO_CHOOSE_LOCATION'); ?>
+    </a><span> (<?php echo Text::_('JCURRENT'); ?>: <?php echo CommunityInfoHelper::getLocation($params, 'label'); ?>)</span>
+  </div>
+</template>
+
+<template id="template-location-modal-body">
+  <form action="<?php echo $currentURL; ?>" method="post" enctype="multipart/form-data" name="adminForm" id="location-form" class="form-validate p-3" aria-label="<?php echo Text::_('MOD_COMMUNITY_INFO_CHOOSE_LOCATION'); ?>">
+    <div class="row">
+      <div class="col-12">
+        <div id="map" class="mb-3" style="height:40vh;width:100%;"></div>
+        <div class="control-group">
+          <div class="control-label"><label for="jform_lat" id="jfrom_lat-lbl">Latitude</label></div>
+          <div class="controls"><input id="jform_lat" class="from-control" type="text" name="jform[lat]"></div>
+        </div>
+        <div class="control-group">
+          <div class="control-label"><label for="jform_lng" id="jfrom_lng-lbl">Longitude</label></div>
+          <div class="controls"><input id="jform_lng" class="from-control" type="text" name="jform[lng]"></div>
+        </div>
+      </div>
+    </div>
+  </form>
+</template>
 
 <?php
 // Location form modal
 $options = array('modal-dialog-scrollable' => true,
                   'title'  => Text::_('MOD_COMMUNITY_INFO_CHOOSE_LOCATION'),
-                  'footer' => '<a class="btn" href="">'.Text::_('MOD_COMMUNITY_INFO_SAVE_LOCATION').'</a><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">'.Text::_('JCLOSE').'</button>',
+                  'footer' => '<a class="btn" href="">'.Text::_('MOD_COMMUNITY_INFO_SAVE_LOCATION').'</a>',
                 );
-$body     = '<form action="'.$currentURL.'" method="post" enctype="multipart/form-data" name="adminForm" id="location-form" class="form-validate" aria-label="'.Text::_('MOD_COMMUNITY_INFO_CHOOSE_LOCATION').'">';
-$body    .=    '<div class="row"><div class="col-12 col-lg-6">';
-$body    .=       '<div id="map" style="height:70vh;width:100%;"></div>';
-$body    .=       '<div class="control-group">';
-$body    .=          '<div class="control-label">';
-$body    .=             '<label for="jform_lat" id="jfrom_lat-lbl">Latitude</label>';
-$body    .=          '</div>';
-$body    .=          '<div class="controls">';
-$body    .=             '<input id="jform_lat" class="from-control" type="text" name="jform[lat]">';
-$body    .=          '</div>';
-$body    .=       '</div>';
-$body    .=       '<div class="control-group">';
-$body    .=          '<div class="control-label">';
-$body    .=             '<label for="jform_lng" id="jfrom_lng-lbl">Longitude</label>';
-$body    .=          '</div>';
-$body    .=          '<div class="controls">';
-$body    .=             '<input id="jform_lng" class="from-control" type="text" name="jform[lng]">';
-$body    .=          '</div>';
-$body    .=       '</div>';
-$body    .=    '</div></div>';
-$body    .= '</form>';
-
-echo HTMLHelper::_('bootstrap.renderModal', 'location-modal', $options, $body);
+echo HTMLHelper::_('bootstrap.renderModal', 'location-modal', $options, '<p>Loading...</p>');
 ?>
 
 <script>
@@ -142,6 +156,10 @@ echo HTMLHelper::_('bootstrap.renderModal', 'location-modal', $options, $body);
   }
       
   let openModal = function(modalId) {
+    let modal      = document.getElementById(modalId);
+    let modalBody  = modal.querySelector('.modal-body');
+    modalBody.innerHTML  = document.getElementById('template-'+modalId+'-body').innerHTML;
+
     bsmodal = new bootstrap.Modal(document.getElementById(modalId), {keyboard: false});
     bsmodal.show();
 
@@ -149,5 +167,19 @@ echo HTMLHelper::_('bootstrap.renderModal', 'location-modal', $options, $body);
       MapCreate();
       registerEvents();
     }, 300);    
+  }
+
+  var callback = function(){
+    // document ready function;
+    let moduleBody   = document.getElementById('CommunityInfo<?php echo strval($module->id); ?>');
+    let moduleHeader = moduleBody.parentNode.previousElementSibling;
+
+    moduleHeader.appendChild(document.getElementById('template-location-picker').content);
+  }; //end callback
+
+  if(document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
+    callback();
+  } else {
+    document.addEventListener('DOMContentLoaded', callback);
   }
 </script>
